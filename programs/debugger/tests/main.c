0d986ef891f97a35ad932856e810afcee0b3b461
John Bellardo <bellardo@calpoly.edu>
2018-12-07 06:46:27 -0800

    Add a TCP server to the debugging test target

--- programs/debugger/tests/main.c
+++ programs/debugger/tests/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "proclib.h"
 #include "events.h"
+#include <errno.h>
+#include "debug.h"
 
 static struct ProcessData *proc = NULL;
 
@@ -51,9 +53,43 @@ void status_cmd(int socket, unsigned char cmd, void *data,
    PROC_cmd_sockaddr(proc, 0xF1, &result, sizeof(result), fromAddr);
 }
 
+int client_read_cb(int fd, char type, void *arg)
+{
+   char buff[2048];
+   int len;
+
+   len = read(fd, buff, sizeof(len));
+   if (len > 0)
+      write(1, buff, len);
+   if (len < 0 && errno == EAGAIN)
+      return EVENT_KEEP;
+   if (len <= 0) {
+      close(fd);
+      return EVENT_REMOVE;
+   }
+
+   return EVENT_KEEP;
+}
+
+int accept_cb(int fd, char type, void *arg)
+{
+   int client;
+   struct sockaddr_in addr;
+   socklen_t len = sizeof(addr);
+
+   client = accept(fd, &addr, &len);
+   EVT_fd_add(arg, client, EVENT_FD_READ, client_read_cb, arg);
+   EVT_fd_set_name(arg, client, "Client %s:%u",
+         inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+   write(client, "Hello World!\n", 13);
+
+   return EVENT_KEEP;
+}
+
 int main(void)
 {
    void *evt;
+   int accept_fd;
 
    // Initialize the process
    proc = PROC_init("test1", WD_DISABLED);
@@ -62,6 +98,7 @@ int main(void)
       return -1;
    }
    
+   DBG_setLevel(DBG_LEVEL_INFO);
    // Setup a signal event 
    PROC_signal(proc, SIGINT, &signal_handler, proc);
    // PROC_set_cmd_handler(proc, 1, &status_cmd, 0, 0, 0);
@@ -80,6 +117,11 @@ int main(void)
 
    EVT_set_initial_debugger_state(PROC_evt(proc), EDBG_STOPPED);
    //EVT_set_initial_debugger_state(PROC_evt(proc), EDBG_ENABLED);
+
+   accept_fd = socket_tcp_init(12345);
+   EVT_fd_add(PROC_evt(proc), accept_fd, EVENT_FD_READ, accept_cb,
+         PROC_evt(proc));
+   EVT_fd_set_name(PROC_evt(proc), accept_fd, "TCP Test:12345");
 
    EVT_start_loop(PROC_evt(proc));
    
